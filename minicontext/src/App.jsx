39dd3974d8afe2a73d47d43bba2c071d774b0a9d
pooja-369 <pujagoyal369@gmail.com>
2024-06-18 17:53:11 +0530

    create context

---
+++ minicontext/src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import UserContextProvider from './assets/context/UserContextProvider'
+
+function App() {
+
+
+  return (
+    <UserContextProvider >
+       
+    </UserContextProvider>
+  )
+}
+
+export default App;
